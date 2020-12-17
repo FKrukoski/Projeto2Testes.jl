eb1f272e6019bcf59a466836cfb856d47788f573
Franklin Angelo Krukoski <franklin.krukoski@gmail.com>
2020-12-17 16:28:22 -0300

    Testes BFGS-RC

--- scripts/exploring.jl
+++ scripts/exploring.jl
@@ -6,10 +6,12 @@ using LinearAlgebra
 
 #%%
 # nlp = ADNLPModel(x -> (x[1] - 1)^2 + (x[2] - 2)^2 / 4, zeros(2))
-nlp = CUTEstModel("ROSENBR")
+nlp = CUTEstModel("GAUSS2LS")
 
 #%%
-output = bfgs_bl(nlp)
+print(nlp)
+#%%
+output = bfgs_rc(nlp)
 print(output)
 
 #%%
